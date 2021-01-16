3ccf441fb296a97ee36009d006a39c48ae7fd005
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2021-01-15 21:01:24 -0500

    add lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import React from 'react';
+import React,{ Suspense } from 'react';
 import './App.css';
-
-import Main from "./features/Main";
-import User from "./features/User"
 
 import {
   BrowserRouter as Router,
@@ -10,17 +7,26 @@ import {
   Switch,
 } from 'react-router-dom';
 
+// import Main from "./features/Main";
+// import User from "./features/User"
+const Main = React.lazy(() => import("./features/Main"));
+const User = React.lazy(() => import("./features/User"))
+
+
+
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <Main/>
-          </Route>
-          <Route exact path="/:login" component={User}/>
-        </Switch>
-      </div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Main/>
+            </Route>
+            <Route exact path="/:login" component={User}/>
+          </Switch>
+        </div>
+      </Suspense>
     </Router>
   );
 }
